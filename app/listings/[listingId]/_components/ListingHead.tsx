fb8872d6b08c88278389a30cb8c060ba811849b7
psnapyeditor <info.psnapy@gmail.com>
2024-03-25 12:01:03 +0300

    updated property listing image card

--- app/listings/[listingId]/_components/ListingHead.tsx
+++ app/listings/[listingId]/_components/ListingHead.tsx
@@ -27,7 +27,7 @@ const ListingHead: React.FC<ListingHeadProps> = async ({
     <>
       <Heading title={title} subtitle={`${region}, ${country}`} backBtn/>
       <div
-        className={`w-full md:h-[620px] sm:h-[280px] bg-gray-100 h-[260px] overflow-hidden  rounded-xl relative transition duration-300`}
+        className={`w-full md:h-[620px] sm:h-[280px] bg-gray-100 h-[260px]   rounded-xl relative transition duration-300`}
       >
         
 
