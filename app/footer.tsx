9209eb7c675d1c210027f672512fdf88b44f6254
psnapyeditor <info.psnapy@gmail.com>
2024-03-30 21:48:13 +0300

    updated footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,52 +1,31 @@
-// Footer.tsx
-
 import React from 'react';
 
 const Footer: React.FC = () => {
   return (
+    <div className="flex justify-between items-center bg-[#ffffff] pt-80">
+      <footer className="flex justify-center items-center bg-[#f0f0f0] pt-10 pb-10">
+        <div>
+          <p>Copyright © 2024 airnesy. All rights reserved.</p>
+          <p>This site is in Development. Stay tuned for the full release!</p>
+        </div>
 
-    <div  className="
-    items-center
-    bg-[#ffffff]
-    pt-80" >
+        <div>
+          <p>airney.info@gmail.com</p>
+        </div>
 
+        <div>
+          <p>about</p>
+          <p>careers</p>
+        </div>
 
-  
-
-    <footer className="text-center
-                       items-center
-                       bg-[#f0f0f0]
-                      
-                       pt-10
-                       pb-10">
-                        <div> 
-                          <p>Copyright © 2024 airnesy. All rights reserved.</p>
-                          <p>This site is in Development. Stay tuned for the full release!</p>
-                        </div>
-
-
-                        <div>
-                        <p>airney.info@gmail.com</p>
-                        </div>
-
-
-
-                        <div>
-                        <p> about</p>
-                        <p>careers</p>
-                        </div>
-     
-      
- 
-      <p> <span>about</span> <span>careers</span></p>
-      <p>Terms . Sitemap . Privacy . English (US) .  $ USD
-.</p>
-      
-
-
-    </footer>
+        <div>
+          <p>
+            <span>about</span> <span>careers</span>
+          </p>
+          <p>Terms . Sitemap . Privacy . English (US) . $ USD .</p>
+        </div>
+      </footer>
     </div>
-
   );
 };
 
