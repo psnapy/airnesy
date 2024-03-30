538a10936b32ccba9d96150c6f798636967b1f71
psnapyeditor <info.psnapy@gmail.com>
2024-03-30 23:16:14 +0300

    updated footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -11,24 +11,26 @@ import logo from './eastate.gif';
                        const Footer: React.FC = () => {
                          return (
                            <div className='pt-80'>
+
+<div className="container6">
+  <div className="social-media1">
+  
+    <a href="#" className="social-icon"><FaInstagram /></a>
+    <a href="#" className="social-icon"><FaTwitter /></a>
+    <a href="#" className="social-icon"><FaTiktok /></a>
+    <a href="#" className="social-icon"><FaFacebookF /></a>
+    <a href="#" className="social-icon"><FaYoutube /></a>
+    <a href="#" className="social-icon"><FaReddit /></a>
+    <a href="#" className="social-icon"><FaDiscord /></a>
+  </div>
+  <div className="logo">
+  <Image src={logo} alt="Company Logo" width={350} height={250} />
+  </div>
+</div>
+
+                            
                           
-                          <div className="flex items-center  mx-auto  items-center  bg-[#f0f0f0] text-center space-x-4">
-                        <div>
-          {/* Social media icons */}
-          <a href="#" className="hover:text-gray-400"><FaInstagram /></a>
-          <a href="#" className="hover:text-gray-400"><FaTwitter /></a>
-          <a href="#" className="hover:text-gray-400"><FaTiktok /></a>
-          <a href="#" className="hover:text-gray-400"><FaFacebookF /></a>
-          <a href="#" className="hover:text-gray-400"><FaYoutube /></a>
-          <a href="#" className="hover:text-gray-400"><FaReddit /></a>
-          <a href="#" className="hover:text-gray-400"><FaDiscord /></a>
-        </div>
-                <div>
-                <Image src={logo} alt="Company Logo" width={350} height={250} /> 
-                  </div>      
-      
          
-        </div> 
                            <footer className="bg-[#ffffff] text-black py-6 ">
                              <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
 
