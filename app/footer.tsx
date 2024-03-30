e6541dd74393507aa0181788f44a3e16f9d97bd5
psnapyeditor <info.psnapy@gmail.com>
2024-03-31 00:23:53 +0300

    updated footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -14,7 +14,7 @@ import logo from './eastate.gif';
                            <div className='pt-60 pb-60 bg-[#f0f0f0] text-center items-center  '>
                             <h2><b>join our community</b></h2>
 
-                            <div className="flex items-center justify-center space-x-4">
+                            <div className="flex items-center justify-center  pl-10 pr-10  space-x-4">
     <div className="icon-circle"><a href="#" className="hover:text-gray-400"><FaInstagram /></a></div>
     <div className="icon-circle"><a href="#" className="hover:text-gray-400"><FaTwitter /></a></div>
     <div className="icon-circle"><a href="#" className="hover:text-gray-400"><FaTiktok /></a></div>
@@ -33,25 +33,59 @@ import logo from './eastate.gif';
      
 
                              <div>
+                             <h5 className="text-lg font-bold">airnesy.com</h5>
                                  <p>&copy; {new Date().getFullYear()} Airnesy Group. All rights reserved.</p>
                                </div>
+
                                <div className="flex justify-center">
+                               <h1 className="text-lg font-bold">Company</h1>
                                  <ul className="flex space-x-4">
-                                   <li>
+                                   <ul>
                                      <a href="#" className="hover:text-gray-400">About</a>
-                                   </li>
-                                   <li>
-                                     <a href="#" className="hover:text-gray-400">Services</a>
-                                   </li>
-                                   <li>
-                                     <a href="#" className="hover:text-gray-400">Careers</a>
-                                   </li>
+                                   </ul>
+                                   <ul>
+                                     <a href="#" className="hover:text-gray-400">careers</a>
+                                   </ul>
+                                   <ul>
+                                     <a href="#" className="hover:text-gray-400">news</a>
+                                   </ul>
+                                   <ul>
+                                     <a href="#" className="hover:text-gray-400">press</a>
+                                   </ul>
+
                                  </ul>
                                </div>
+
+
+                               <div className="flex justify-center">
+                               <h1 className="text-lg font-bold">Product</h1>
+                                 <ul className="flex space-x-4">
+                                   <ul>
+                                     <a href="#" className="hover:text-gray-400">Web App</a>
+                                   </ul>
+                                   <ul>
+                                     <a href="#" className="hover:text-gray-400">iphone App</a>
+                                   </ul>
+                                   <ul>
+                                     <a href="#" className="hover:text-gray-400">Android App</a>
+                                   </ul>
+                                 </ul>
+                               </div>
+
                                <div className="mb-4 md:mb-0">
-                                 <h1 className="text-lg font-bold">airnesy group</h1>
+                                 <h1 className="text-lg font-bold">Contact </h1>
                                  <p className="mt-2">123 Main Street, NRB, Kenya</p>
                                  <p>Email: airnesy.info@gmail.com</p>
+                                 <p> 
+                                 Support
+                                  </p>
+                                 <p> 
+                                 Privacy Policy
+                                  </p>
+
+<                                   p> 
+                                 erms of Service  
+                                  </p>
                                </div>
                               
                              </div>
