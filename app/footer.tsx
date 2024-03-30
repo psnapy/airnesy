1860f4316b98f3be9be5bde070fd70e3fe319492
psnapyeditor <info.psnapy@gmail.com>
2024-03-31 01:00:50 +0300

    updated footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -33,7 +33,7 @@ import logo from './eastate.gif';
      
 
                              <div>
-                           <h1 className='pt-3   style="font-size: 20px;'><b>airnesy.com</b></h1>
+                           <h1 className='pt-3   style="font-size: 20px;'>airnesy.com</h1>
                                  <p>&copy; {new Date().getFullYear()} Airnesy Group. All rights reserved.</p>
                                </div>
 
