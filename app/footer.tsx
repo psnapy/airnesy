59bb68c104a2b2af76ba11a16488ed49446e8653
psnapyeditor <info.psnapy@gmail.com>
2024-03-30 22:06:40 +0300

    updated footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,9 +1,10 @@
+
 import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <div className="flex justify-between items-center bg-[#ffffff] pt-80 width-full" >
-      <footer className="flex justify-center items-center bg-[#f0f0f0] pt-10 pb-10 pl-10 pr-10">
+    <div className="flex justify-between items-center bg-[#ffffff] pt-80 w-full">
+      <footer className="flex justify-center items-center bg-[#f0f0f0] pt-10 pb-10 px-5">
         <div className='text-center p-5'>
           <p>Copyright © 2024 airnesy. All rights reserved.</p>
           <p>This site is in Development. Stay tuned for the full release!</p>
