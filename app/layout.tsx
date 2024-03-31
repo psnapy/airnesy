8e25bdfdd9110945cc7f2108067ae7f6d7a66b49
psnapyeditor <info.psnapy@gmail.com>
2024-03-31 13:12:50 +0300

    added about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,70 +1,58 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
-import "react-loading-skeleton/dist/skeleton.css";
-import Navbar from "@/components/navbar";
-
-import Providers from "@/components/Provider";
+// Import necessary modules from Next.js and React
+import { useRouter } from "next/router";
 import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
- 
-import { useRouter } from "next/router";
+import { Nunito } from "next/font/google";
+import Navbar from "@/components/navbar";
+import Providers from "@/components/Provider";
 import Footer from "./footer";
 
+// Define the Nunito font with specified subsets
 const nunito = Nunito({ subsets: ["latin"] });
 
-
+// Define the RootLayout component
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // Get the router instance
   const router = useRouter();
 
   // Determine if the current page is the About page
   const isAboutPage = router.pathname === "/about";
-    
-       <html lang="en">
 
+  return (
+    <html lang="en">
       <Head>
         <title>Airnesy: Buy & Rent Real Estate, Apartments, homes & Comercial property</title>
         <meta
           name="description"
-          content="The leading real eastate maeketplace. Your ultimate Home and Property Finder, Discover a world of endless property listings and find your dream home in a click."
+          content="The leading real estate marketplace. Your ultimate Home and Property Finder, Discover a world of endless property listings and find your dream home in a click."
         />
-      <script type="application/ld+json">
-          {`{
+        {/* JSON-LD script for Organization */}
+        <script type="application/ld+json">
+          {JSON.stringify({
             "@context": "http://schema.org",
             "@type": "Organization",
             "name": "Airnesy Group",
             "logo": "https://res.cloudinary.com/dcrqqqget/image/upload/v1711821828/favicon_zpzigj.jpg",
             "url": "https://www.airnesy.com"
-          }` as any}
+          })}
         </script>
-        
       </Head>
 
-
-    
-
-     
-      
       <body className={nunito.className}>
+        {/* Wrap the content with Providers */}
         <Providers>
           <Navbar />
+          {/* Render main content */}
           <main className="pb-16 md:pt-28 pt-24">{children}</main>
-
-
-          <Footer/>
+          <Footer />
         </Providers>
-       
+        {/* Include analytics */}
         <Analytics />
       </body>
-
-
-
     </html>
-  
   );
 }
-
