91e3b204895762b180d7547b2e1ee0b7314dff0c
psnapyeditor <info.psnapy@gmail.com>
2024-03-31 13:33:09 +0300

    added about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,69 @@
-// Import necessary modules from Next.js and React
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import Navbar from "@/components/navbar";
+
+import Providers from "@/components/Provider";
 import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/react';
-import { Nunito } from "next/font/google";
-import Navbar from "@/components/navbar";
-import Providers from "@/components/Provider";
+ 
 import Footer from "./footer";
 
-// Define the Nunito font with specified subsets
 const nunito = Nunito({ subsets: ["latin"] });
 
-// Define the RootLayout component
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+
+
+    
+       <html lang="en">
+
       <Head>
         <title>Airnesy: Buy & Rent Real Estate, Apartments, homes & Comercial property</title>
         <meta
           name="description"
-          content="The leading real estate marketplace. Your ultimate Home and Property Finder, Discover a world of endless property listings and find your dream home in a click."
+          content="The leading real eastate maeketplace. Your ultimate Home and Property Finder, Discover a world of endless property listings and find your dream home in a click."
         />
-        {/* JSON-LD script for Organization */}
-        <script type="application/ld+json">
-          {JSON.stringify({
+      <script type="application/ld+json">
+          {`{
             "@context": "http://schema.org",
             "@type": "Organization",
             "name": "Airnesy Group",
             "logo": "https://res.cloudinary.com/dcrqqqget/image/upload/v1711821828/favicon_zpzigj.jpg",
             "url": "https://www.airnesy.com"
-          })}
+          }` as any}
         </script>
+        
       </Head>
 
+
+    
+
+     
+      
       <body className={nunito.className}>
-        {/* Wrap the content with Providers */}
         <Providers>
           <Navbar />
-          {/* Render main content */}
           <main className="pb-16 md:pt-28 pt-24">{children}</main>
-          <Footer />
+
+
+          <Footer/>
         </Providers>
-        {/* Include analytics */}
+       
         <Analytics />
       </body>
+
+
+
     </html>
+  
   );
 }
+
