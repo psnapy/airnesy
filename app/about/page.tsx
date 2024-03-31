b8220ca2c1b498cc7e156354b4d67192a1a076ac
psnapyeditor <info.psnapy@gmail.com>
2024-03-31 13:41:58 +0300

    added about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,25 +1,27 @@
 import React from "react";
-import EmptyState from "@/components/EmptyState";
+import NoNavbarLayout from "../nonavbarlayout";
 import Heading from "@/components/Heading";
 
 const AboutPage = () => {
   return (
-    <section className="main-container3">
-      <Heading
-        title="About Us"
-        subtitle="Learn more about our company."
-        backBtn={false}
-      />
-      <div className="mt-8">
-        <p className="text-lg mb-4">
-          Welcome to our website! We are dedicated to providing excellent services.
-        </p>
-        <p className="text-lg">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
-        </p>
-        {/* Add more content as needed */}
-      </div>
-    </section>
+    <NoNavbarLayout>
+      <section className="main-container3">
+        <Heading
+          title="About Us"
+          subtitle="Learn more about our company."
+          backBtn={false}
+        />
+        <div className="mt-8">
+          <p className="text-lg mb-4">
+            Welcome to our website! We are dedicated to providing excellent services.
+          </p>
+          <p className="text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.
+          </p>
+          {/* Add more content as needed */}
+        </div>
+      </section>
+    </NoNavbarLayout>
   );
 };
 
---
+++ app/nonavbarlayout.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from "react";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import Providers from "@/components/Provider";
+import Head from 'next/head';
+import { Analytics } from '@vercel/analytics/react';
+import Footer from "./footer";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+interface NoNavbarLayoutProps {
+  children: ReactNode;
+}
+
+const NoNavbarLayout = ({ children }: NoNavbarLayoutProps) => {
+  return (
+    <html lang="en">
+      <Head>
+        <title>Airnesy: Buy & Rent Real Estate, Apartments, homes & Comercial property</title>
+        <meta
+          name="description"
+          content="The leading real eastate maeketplace. Your ultimate Home and Property Finder, Discover a world of endless property listings and find your dream home in a click."
+        />
+        <script type="application/ld+json">
+          {`{
+            "@context": "http://schema.org",
+            "@type": "Organization",
+            "name": "Airnesy Group",
+            "logo": "https://res.cloudinary.com/dcrqqqget/image/upload/v1711821828/favicon_zpzigj.jpg",
+            "url": "https://www.airnesy.com"
+          }`}
+        </script>
+      </Head>
+      <body className={nunito.className}>
+        <Providers>
+          <main className="pb-16 md:pt-28 pt-24">{children}</main>
+          <Footer />
+        </Providers>
+        <Analytics />
+      </body>
+    </html>
+  );
+};
+
+export default NoNavbarLayout;
