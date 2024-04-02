1797364cd0450ed7fd3774ec6b54ff01cfbe7fee
psnapyeditor <info.psnapy@gmail.com>
2024-04-02 14:25:00 +0300

    added about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -96,7 +96,7 @@ const AboutPage = () => {
     src="/images/a.jpg"
     alt="Left Image"
      // Adjust width as needed
-    height={800} // Remove the height attribute or set it to null
+    height={500} // Remove the height attribute or set it to null
   />
 </div>
 
@@ -109,7 +109,7 @@ const AboutPage = () => {
     src="/images/a.jpg"
     alt="Right Image"
      // Adjust width as needed
-    height={800} // Remove the height attribute or set it to null
+    height={500} // Remove the height attribute or set it to null
   />
 </div>
 
