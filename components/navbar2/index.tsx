3a40332220548db5a2367ea07ed4255f52c9fc2e
psnapyeditor <info.psnapy@gmail.com>
2024-04-02 20:06:30 +0300

    added about page

--- components/navbar2/index.tsx
+++ components/navbar2/index.tsx
@@ -7,23 +7,23 @@ const Navbar: React.FC<NavbarProps> = async () => {
   const user = await getCurrentUser();
 
   return (
-    <header className="fixed top-0 left-0 w-full bg-white z-10 ">
-      <nav className="py-3 border-b-[1px]">
+    <header className="fixed top-0 left-0 w-full bg-white z-10 pt-2 ">
+      <nav className="py-3 ">
         <div className="flex main-container5 flex-row justify-between items-center gap-3 md:gap-0">
         <p>
 
-<Link href="https://www.airnesy.com/" className="hover:text-gray-900  fontsizee" >
+<Link href="https://www.airnesy.com/" className="hover:text-gray-900  fontsizee pl-2 " >
   <a> <b>airnesy.com</b></a>
 </Link>
 </p>
 
-<div className="pl-4">
+<div className="pl-4 ">
             <Link href="https://www.airnesy.com/">
                 <a className="buttonnnn ">Careers</a>
               </Link>
 
               
-            <Link href="https://www.airnesy.com/" className="pl-4">
+            <Link href="https://www.airnesy.com/" className="pl-4 ">
                 <a className="buttonnn ">Try it out</a>
               </Link>
 
