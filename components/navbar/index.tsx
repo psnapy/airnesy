2764bf45c2aa12d1206c21ed99728530a0a5808d
psnapyeditor <info.psnapy@gmail.com>
2024-04-03 02:55:45 +0300

    first commit

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC<NavbarProps> = async () => {
 
   return (
     <header className="fixed top-0 left-0 w-full bg-white z-10 ">
-      <nav className="py-3 border-b-[1px]">
+      <nav className="py-3 border-b-[1px]-hidden sm:block  ">
         <div className="flex main-container flex-row justify-between items-center gap-3 md:gap-0">
           <Logo />
           <Suspense fallback={<></>}>
